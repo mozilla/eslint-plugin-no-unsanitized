c1315eb25b6988e8f300530c15c5db87c84da2b7
Frederik Braun <fbraun+gh@mozilla.com>
2015-09-09 11:25:11 +0200

    Bug 1198200: eslint gives up on some simple innerHTML assignments

--- tests/rules/no-unsafe-innerhtml.js
+++ tests/rules/no-unsafe-innerhtml.js
@@ -215,6 +215,17 @@ eslintTester.run("no-unsafe-innerhtml", rule, {
                     type: "CallExpression"
                 }
             ]
-        }
+        },
+        // bug https://bugzilla.mozilla.org/show_bug.cgi?id=1198200
+        {
+            code: "title.innerHTML = _('WB_LT_TIPS_S_SEARCH'," +
+            " {value0:engine});",
+            errors: [
+                {
+                    message: "Unsafe assignment to innerHTML",
+                    type: "AssignmentExpression"
+                }
+            ]
+}
     ]
 });
