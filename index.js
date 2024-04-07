6fad509ed5bed12b4e42577325bf2544bc7163e9
Mark Banner <standard8@mozilla.com>
2024-04-07 13:57:22 +0100

    Update plugin configuration to export in ESLint v8 and v9 formats.

--- index.js
+++ index.js
@@ -1,46 +1,64 @@
-module.exports = {
+const packageJSON = require("./package.json");
+
+const plugin = {
+    meta: {
+        name: "eslint-plugin-no-unsanitized",
+        version: packageJSON.version,
+    },
     rules: {
         property: require("./lib/rules/property"),
         method: require("./lib/rules/method"),
     },
-    configs: {
-        DOM: {
-            plugins: ["no-unsanitized"],
-            rules: {
-                "no-unsanitized/property": [
-                    "error",
-                    {},
-                    {
-                        // Check unsafe assignment to innerHTML
-                        innerHTML: {},
+    configs: {},
+};
 
-                        // Check unsafe assignment to outerHTML
-                        outerHTML: {},
-                    },
-                ],
-                "no-unsanitized/method": [
-                    "error",
-                    {},
-                    {
-                        // check second parameter to .insertAdjacentHTML()
-                        insertAdjacentHTML: {
-                            properties: [1],
-                        },
+const rules = {
+    "no-unsanitized/property": [
+        "error",
+        {},
+        {
+            // Check unsafe assignment to innerHTML
+            innerHTML: {},
 
-                        // check first parameter to .write(), as long as the preceeding object matches the regex "document"
-                        write: {
-                            objectMatches: ["document"],
-                            properties: [0],
-                        },
+            // Check unsafe assignment to outerHTML
+            outerHTML: {},
+        },
+    ],
+    "no-unsanitized/method": [
+        "error",
+        {},
+        {
+            // check second parameter to .insertAdjacentHTML()
+            insertAdjacentHTML: {
+                properties: [1],
+            },
 
-                        // check first parameter to .writeLn(), as long as the preceeding object matches the regex "document"
-                        writeln: {
-                            objectMatches: ["document"],
-                            properties: [0],
-                        },
-                    },
-                ],
+            // check first parameter to .write(), as long as the preceeding object matches the regex "document"
+            write: {
+                objectMatches: ["document"],
+                properties: [0],
+            },
+
+            // check first parameter to .writeLn(), as long as the preceeding object matches the regex "document"
+            writeln: {
+                objectMatches: ["document"],
+                properties: [0],
             },
         },
-    },
+    ],
 };
+
+Object.assign(plugin.configs, {
+    "DOM-legacy": {
+        plugins: ["no-unsanitized"],
+        rules,
+    },
+    DOM: [
+        {
+            plugins: { "no-unsanitized": plugin },
+            rules,
+        },
+    ],
+});
+
+module.exports = plugin;
