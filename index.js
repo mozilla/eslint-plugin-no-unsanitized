5acee02bfa4f532fcc81745ae39aeb87f6518321
Matt Mower <mdmower@cmphys.com>
2025-11-05 13:57:00 -0800

    Improve inferred type for type checked eslint configs

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const path = require("path");
 const data = readFileSync(path.join(__dirname, "package.json"));
 const packageJSON = JSON.parse(data);
 
+/** @type {Record<string, "off" | "warn" | "error">} */
+const rules = {
+    "no-unsanitized/property": "error",
+    "no-unsanitized/method": "error",
+};
+
 const plugin = {
     meta: {
         name: "eslint-plugin-no-unsanitized",
@@ -13,33 +19,25 @@ const plugin = {
         property: require("./lib/rules/property"),
         method: require("./lib/rules/method"),
     },
-    configs: {},
+    configs: {
+        "recommended-legacy": {
+            plugins: ["no-unsanitized"],
+            rules,
+        },
+        recommended: {
+            plugins: { "no-unsanitized": {} },
+            rules,
+        },
+        get DOM() {
+            console.log(
+                'The "DOM" configuration of the "no-unsanitized" plugin is deprecated. Use "recommended-legacy" instead.'
+            );
+
+            return plugin.configs["recommended-legacy"];
+        },
+    },
 };
 
-const rules = {
-    "no-unsanitized/property": "error",
-    "no-unsanitized/method": "error",
-};
-
-Object.assign(plugin.configs, {
-    "recommended-legacy": {
-        plugins: ["no-unsanitized"],
-        rules,
-    },
-    recommended: {
-        plugins: { "no-unsanitized": plugin },
-        rules,
-    },
-});
-Object.defineProperty(plugin.configs, "DOM", {
-    enumerable: true,
-    get() {
-        console.log(
-            'The "DOM" configuration of the "no-unsanitized" plugin is deprecated. Use "recommended-legacy" instead.'
-        );
-
-        return this["recommended-legacy"];
-    },
-});
+plugin.configs.recommended.plugins["no-unsanitized"] = plugin;
 
 module.exports = plugin;
